1e09e2e91bb2a7227f65250afd491a4f08bd18b9
Harlan Sang <harlan.sang@linkedmink.net>
2025-07-22 11:44:57 -0500

    feat: Add wireguard scripts for qbittorrent

--- qbittorrent-wireguard/docker-entrypoint.sh
+++ qbittorrent-wireguard/docker-entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+IP_CHANGE_RETRY_LIMIT="${IP_CHANGE_RETRY_LIMIT:-5}"
+
 set -e
 
-exec qbittorrent-nox --confirm-legal-notice
+ORIGINAL_IP=$(curl "https://ipinfo.io/ip")
+export ORIGINAL_IP
+echo "ORIGINAL_IP: ${ORIGINAL_IP}"
+
+wg-quick up /run/secrets/wg0.conf
+
+retries=0
+currentIp=$ORIGINAL_IP
+while [ "$currentIp" = "$ORIGINAL_IP" ]
+do
+  if [ "$retries" -gt "$IP_CHANGE_RETRY_LIMIT" ]; then
+    echo "Retry limit exceeded: ${IP_CHANGE_RETRY_LIMIT}"
+  fi
+  sleep 1
+
+  currentIp=$(curl "https://ipinfo.io/ip")
+
+  retries=$((retries+1))
+done
+
+echo "currentIp: ${currentIp}"
+
+exec su -c "qbittorrent-nox --confirm-legal-notice" -- torrent
